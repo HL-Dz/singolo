72874b395029c1162530c1c5f9f564835cd8ee7b
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-03-13 21:47:05 +0300

    feat: add switch tabs and images interaction

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const NAV = document.querySelector('.header__nav');
+const portfolioBtns = document.querySelector('ul.buttons');
+const portfolioBlock = document.querySelector('.portfolio__images');
 
+
+// Switch menu
 NAV.addEventListener('click', (e)=> {
   document.querySelectorAll('a').forEach((elem)=> {
-    elem.classList.remove('active')
+    elem.classList.remove('active');
   })
   e.target.classList.add('active');
-})
+});
+
+
+// Switch tabs
+portfolioBtns.addEventListener('click', (e)=> {
+  let lastImage = portfolioBlock.lastElementChild;
+  document.querySelectorAll('a').forEach(elem=> {
+    elem.classList.remove('active');
+  })
+  if(e.target.tagName === 'A') {
+    e.preventDefault();
+    e.target.classList.add('active');
+    lastImage.remove();
+    portfolioBlock.insertAdjacentElement('afterbegin', lastImage);
+  }
+});
+
+
+// Images interaction
+portfolioBlock.addEventListener('click', (e)=> {
+  if(e.target.classList.contains('portfolio__img')) {
+    document.querySelectorAll('.portfolio__img').forEach(el=> {
+      el.classList.remove('portfolio__img-active')
+    });
+    e.target.classList.add('portfolio__img-active');
+  }
+});
