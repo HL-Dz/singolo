c0b7ee8512c02fdd191c1fbaedaa5b818179d14b
Dzmitry Hlushak <hlushak.dzmitry@gmail.com>
2020-03-14 23:35:59 +0300

    feat: add message popup

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const NAV = document.querySelector('.header__nav');
 const portfolioBtns = document.querySelector('ul.buttons');
 const portfolioBlock = document.querySelector('.portfolio__images');
-
+const messageBlock = document.getElementById('message-block');
+const name = document.getElementById('form-name');
+const email = document.getElementById('form-email');
+const subject = document.getElementById('form-subject');
+const describe = document.getElementById('form-detail');
+const formSubmit = document.getElementById('form-submit');
+const closeBtn = document.getElementById('close-btn');
 
 // Switch menu
 NAV.addEventListener('click', (e)=> {
@@ -36,3 +42,45 @@ portfolioBlock.addEventListener('click', (e)=> {
     e.target.classList.add('portfolio__img-active');
   }
 });
+
+
+// Message popup
+const showSentMessage = (e) => {
+  if(name.value !== '' && email.value !== '') {
+    e.preventDefault();
+    let subject = document.querySelector('#form-subject').value.toString();
+    let describe = document.querySelector('#form-detail').value.toString();
+    setTimeout(() => {
+      if(subject !== '') {
+        document.getElementById('result-subject').innerText = subject;
+      } else {
+        document.getElementById('result-subject').innerText = 'Без темы'
+      }
+    
+      if(describe !== '') {
+        document.getElementById('result-detail').innerText = describe;
+      } else {
+        document.getElementById('result-detail').innerText = 'Без описания';
+      }
+      messageBlock.classList.remove('popup-hide');
+      messageBlock.classList.remove('hidden');
+      messageBlock.classList.add('popup-show');
+    }, 500);
+  }
+}
+
+// Hide Popup
+const hidePopup = () => {
+  setTimeout(() => {
+    messageBlock.classList.remove('popup-show');
+    messageBlock.classList.add('popup-hide');
+    messageBlock.classList.add('hidden');
+    document.querySelector('#form-name').value = '';
+    document.querySelector('#form-email').value = '';
+    document.querySelector('#form-subject').value = '';
+    document.querySelector('#form-detail').value = '';
+  }, 500);
+}
+
+closeBtn.addEventListener('click', hidePopup);
+formSubmit.addEventListener('click', showSentMessage);
